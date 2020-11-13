a2c93cb8758da16fdae6f0beef542e6263fed02b
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-13 12:23:45 +0700

    Update show.php

--- show.php
+++ show.php
@@ -12,7 +12,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table class="table table-bordered">
+<table class="table table-striped">
   <thead>
     <tr>
       <th width="100"> <div align="center">name</div></th>
