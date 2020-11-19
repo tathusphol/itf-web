5af82c78eb8b81adc354b32dc737569d87c2fd90
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-19 22:11:17 +0700

    Update del.php

--- ITF/ITF/del.php
+++ ITF/ITF/del.php
@@ -1,8 +1,8 @@
 <?php
-$id=$_GET['Id'];
+$id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'tathus.mysql.database.azure.com', 'tathus@tathus', 'SakuraHiro8852', 'itflab', 3306);
-$sql="DELETE FROM guestbook WHERE Id='$id'";
+$sql="DELETE FROM guestbook WHERE ID='$id'";
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
