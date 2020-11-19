796ea9673f8210dae5e57a5799e44b8a10bc4e58
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-19 22:12:21 +0700

    Update update.php

--- ITF/ITF/update.php
+++ ITF/ITF/update.php
@@ -1,11 +1,11 @@
 <?php
-$id=$_GET['Id'];
+$id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'tathus.mysql.database.azure.com', 'tathus@tathus', 'SakuraHiro8852', 'itflab', 3306);
 $name=$_POST['Name'];
 $comment=$_POST['Comment'];
 $link=$_POST['Link'];
-$sql="UPDATE guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE Id='$id'";
+$sql="UPDATE guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE ID='$id'";
 if (mysqli_query($conn, $sql)) {
     header("Location: show.php");
   } else {
