7575cc09f77cbd83ea17b3229c48701061d68242
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-19 19:38:31 +0700

    Update edit.php

--- ITF/edit.php
+++ ITF/edit.php
@@ -21,11 +21,11 @@
     <h2>Edit Form</h2>
     <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
         <label for="Username">Username :</label>
-          <input type="text" class="form-control" name = "name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
+          <input type="text" class="form-control" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
           <label for="Username">Comment :</label>
-          <input type="text" class="form-control" name = "comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
+          <input type="text" class="form-control" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
         <label for="Username">Link :</label>
-          <input type="text" class="form-control" name = "link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
+          <input type="text" class="form-control" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
           <input type="submit" id="commentBtn"class="btn btn-outline-warning">
     </div>
       </form>
