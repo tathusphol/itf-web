25f546a6880bc2a22cac5a1ebe6591338330c4ed
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-27 00:50:04 +0700

    Update connect.php

--- lol/connect.php
+++ lol/connect.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, '127.0.0.1', 'root', '', 'testitf', 3306);
+mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070065_it63070065', 'BDIxdi17', 'it63070065_it63070065', 3306);
 if(mysqli_connect_errno($conn)){
     echo "connect fail".mysqli_connect_error();
 }
