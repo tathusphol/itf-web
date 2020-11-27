5ad081ec9cee519a7e952edc7c601465ca108e7a
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-27 11:48:39 +0700

    Update delete.php

--- labitf/delete.php
+++ labitf/delete.php
@@ -4,7 +4,7 @@ $id=$_GET['id'];
 $sql="DELETE FROM labtest WHERE id='$id'";
 if (mysqli_query($conn, $sql)) {
     echo "delete data successfully";
-    echo "<button type="button" class="btn btn-outline-warning" onclick="window.location.href='index.php'">Home page</button>";
+    echo "<a href='index.php'> Return to home page</a>";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
