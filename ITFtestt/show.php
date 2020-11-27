a98f955b62d6ace1e8bbe3a079d4f97a5a43a5ce
tathusphol <73683391+tathusphol@users.noreply.github.com>
2020-11-27 10:04:06 +0700

    Update show.php

--- ITFtestt/show.php
+++ ITFtestt/show.php
@@ -12,6 +12,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SHOW</title>
+   <style>
+      table, th, td {border: 1px solid black;}
+    </style>
 </head>
 <body>
     <table>
